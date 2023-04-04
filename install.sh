7eb57e680f2bea0544e47e8448d8f408eed219cf
aidan@electedtechnologies.co.uk <aidan>
2023-04-04 17:06:38 +0100

    created install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_NAME="create-react-component"     # Replace with the desired alias name
+
+# Check if the alias already exists
+ALIAS_EXISTS=$(grep -ic "$SCRIPT_NAME=" ~/.bashrc)
+
+# Copy the script to the /usr/local/bin directory
+sudo cp $SCRIPT_NAME /usr/local/bin
+
+# Make the script executable
+sudo chmod +x /usr/local/bin/$SCRIPT_NAME
+
+if [ $ALIAS_EXISTS -eq 0 ]; then
+  # Add the alias to the .bashrc file
+  echo "alias $SCRIPT_NAME='/usr/local/bin/$SCRIPT_NAME'" >> ~/.bashrc
+
+  # Apply the changes to the current terminal session
+  source ~/.bashrc
+
+  echo "Alias created successfully!"
+else
+  echo "The alias '$SCRIPT_NAME' already exists in your .bashrc file."
+fi
